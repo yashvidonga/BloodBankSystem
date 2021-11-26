0e701b50fcb2e61c835dceb5fd954ca2e3ddb1ff
yashvidonga29 <yashvidonga29@gmail.com>
2021-11-26 17:08:46 +0530

    project uploaded

---
+++ BloodBankInterface/src/bloodbankinterface/BloodBankInterface.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bloodbankinterface;
+import InterfacePages.Login;
+/**
+ *
+ * @author Yashvi
+ */
+public class BloodBankInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login form1 = new Login();
+        form1.setVisible(true);
+    }
+    
+}
